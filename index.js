e6f0d6a1b64ceb961a67b786d53186e86c3bbd3d
AmonCheruiyot <161847144+AmonCheruiyot@users.noreply.github.com>
2024-03-25 16:39:47 +0300

    completes test

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const reducer = (totalBatteries, currentBatch) => totalBatteries + currentBatch;
+
+const totalBatteries = batteryBatches.reduce(reducer, 0);
+
+console.log(totalBatteries);
+
+module.exports = totalBatteries;
